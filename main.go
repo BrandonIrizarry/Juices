b3bdef866bf061d7fd05cd65d176d93688bd81a6
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-06-12 18:31:57 -0400

    feat: add basic server code

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	// Set up server.
+	mux := http.NewServeMux()
+
+	// For now, we need this primarily to load HTMX.
+	loadJS := http.StripPrefix("/js/", http.FileServer(http.Dir("./js")))
+	mux.Handle("GET /js/", loadJS)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
