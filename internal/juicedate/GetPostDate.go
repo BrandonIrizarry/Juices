cb8a22c2743e8853d9afb0ce21fc2fc4cb7ed5a6
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-06-12 21:30:10 -0400

    feat: add counting widget for the given juice

--- internal/juicedate/GetPostDate.go
+++ internal/juicedate/GetPostDate.go
@@ -15,6 +15,7 @@ var postDateHTML = strings.TrimSpace(`
 <div>
 <span>%s</span>
 <button id="edit" hx-get="/date" hx-swap="outerHTML" hx-target="closest div">Edit</button>
+<input type="number" id="count" name="count" min=0 />
 <button id="delete" hx-delete="/date" hx-swap="delete" hx-target="closest div">Delete</button>
 </div>
 %s`)
