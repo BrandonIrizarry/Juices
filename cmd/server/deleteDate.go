bedd411d1d3a57fbe2e0eaeea393c4561488a5b1
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-06-15 20:27:40 -0400

    feat: make delete-button clear associated map entry

--- cmd/server/deleteDate.go
+++ cmd/server/deleteDate.go
@@ -3,8 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func deleteDate(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving %s %s\n", r.Method, r.URL.Path)
+
+	// The key of the map entry we're deleting has precisely an id
+	// of one less than that of this delete button.
+	id, err := strconv.Atoi(r.Header.Get("Hx-Trigger"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	delete(counts, id-1)
+
+	log.Printf("Counts: %v\n", counts)
 }
